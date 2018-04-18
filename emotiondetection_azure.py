d95afcdddb37ace4563b61a1ac2772e9d144ee38
nanoyulian <nanoyulian@gmail.com>
2018-04-18 11:58:31 +0700

    klasifikasi emosi ke 3 jenis neg,net,pos dan insert ke thinkspeak
    tabel di thinkspeak masih lama kolomnya belum diubah

--- emotiondetection_azure.py
+++ emotiondetection_azure.py
@@ -1,6 +1,7 @@
 ########### Python 2.7 #############
 #import library
 import requests
+import operator
 
 def detect_emotion_rate(list_emotions):
     
@@ -8,6 +9,7 @@ def detect_emotion_rate(list_emotions):
 
 ################## ENTRY POINT #################################
 if __name__ == "__main__":
+
     #key untuk 30hari
     subscription_key = "6214eaf40be54d2fa87acc86ae7acab1"
     assert subscription_key
@@ -23,17 +25,69 @@ if __name__ == "__main__":
         'returnFaceLandmarks': 'false',
         'returnFaceAttributes': 'age,gender,emotion',
     }
-    
+    #execute API Url    
     response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+    #jumlah face yang dideteksi    
     num_faces = len(response.json())
     #print num_faces
-    #print response.json()[0]
+    
+    # kelompok mood : 
+    # negative = sadness, disgust, contempt, anger, fear
+    list_sad = []
+    list_contempt = []
+    list_anger = []
+    list_fear = []
+    list_disgust = []
+    # netral = netral
+    list_neutral = []
+    # positive = happiness, surprise    
+    list_happy = []
+    list_surprise = []
+    
+    #tambahkan masing-masing jenis emosi dr semua face ke dalam list jenis emosi msg2x
+    for i in range(0,num_faces): 
+        list_sad.append(response.json()[i]['faceAttributes']['emotion']['sadness'])
+        list_disgust.append(response.json()[i]['faceAttributes']['emotion']['disgust'])
+        list_contempt.append(response.json()[i]['faceAttributes']['emotion']['contempt'])
+        list_anger.append(response.json()[i]['faceAttributes']['emotion']['anger'])
+        list_fear.append(response.json()[i]['faceAttributes']['emotion']['fear'])
+        list_neutral.append(response.json()[i]['faceAttributes']['emotion']['neutral'])
+        list_surprise.append(response.json()[i]['faceAttributes']['emotion']['surprise'])
+        list_happy.append(response.json()[i]['faceAttributes']['emotion']['happiness'])
+    
+    #hitung rata-rata emosi dari semua face per jenis emosi
+    #tambahkan dan klasifikasikan ke dalam kategori negatif, netral, dan positif 
+    rate_sad = sum(list_sad) / float(len(list_sad))
+    rate_contempt = sum(list_contempt) / float(len(list_contempt))
+    rate_anger = sum(list_anger) / float(len(list_anger))
+    rate_fear = sum(list_fear) / float(len(list_fear))
+    rate_disgust = sum(list_disgust) / float(len(list_disgust))
+    #rata-rata emosi negatif
+    rate_negative = (rate_sad + rate_contempt + rate_anger + rate_disgust) / 4.0 
+    #rataan emosi netral
+    rate_neutral = sum(list_neutral) / float(len(list_neutral))
+    #rataan emosi positif
+    rate_happy = sum(list_happy) / float(len(list_happy))
+    rate_surprise = sum(list_surprise) / float(len(list_surprise))
+    rate_positive = rate_happy + rate_surprise / 2.0 
+    
+    #simpan hasilnya nilai rataan dalam dictionary kategori emosi
+    dict_group_emotions = {'negative' : rate_negative, 'neutral' : rate_neutral, 'positive' : rate_positive }
+    emotion_detected = max(dict_group_emotions.iteritems(), key=operator.itemgetter(1))[0]
+    print "Emotion : ", emotion_detected
     #kolom yang diambil faceId, faceAttributes[emotion], gender, age
     
+    
+    #insert data ke thingspeak (database online service API) SESUAIKAN KOLOM2XNYA.. INI MASIH ADA JENIS KELAMINNYA JADI DIISI undefined_jeniskel
+    url_insert = "https://api.thingspeak.com/update?api_key=9DV9Y5S7FKZAR5P0&field1=" + 'undefined_jenisKel' + "&field2=" + emotion_detected
+    #proses timeoout dalam second
+    data = {
+        'timeout': 60
+    }   
+    response = requests.post(url_insert,data=data)
+    
+    print "status insert ", response
     #hitung rata-rata emotion dari semua faceid
-    emotion_rate = detect_emotion_rate()
-
-
-
+    #emotion_rate = detect_emotion_rate()
 
 ####################################
